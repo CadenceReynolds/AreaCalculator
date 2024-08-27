fcf7388785d9402cc319c94e9d2bb3b56b179ca1
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-27 13:00:38 -0400

    Done2

--- src/areaCalculator.java
+++ src/areaCalculator.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class areaCalculator {
+public class areaCalculator {
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(-1));
+        System.out.println(area(5.0, 4.0));
+        System.out.println(area(-1.0, 4.0));
+    }
+
+    public static double area(double num1, double num2){
+        if(num1 < 0){
+            return -1.0;
+        } else if(num2 <0){
+            return -1.0;
+        }
+        return num1 * num2;
+    }
+
+    public static double area(double num1){
+        if(num1 < 0){
+            return -1.0;
+        }
+        return (num1 * num1) * Math.PI;
+    }
 }
